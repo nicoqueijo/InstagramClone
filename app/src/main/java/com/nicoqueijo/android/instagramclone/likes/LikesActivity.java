f0cbf4fabc6d95f36a3f3c7d06f476a780a7ebad
Nicolas Queijo <queijonicolas@gmail.com>
2018-10-10 19:22:46 -0400

    completed part 19

--- app/src/main/java/com/nicoqueijo/android/instagramclone/likes/LikesActivity.java
+++ app/src/main/java/com/nicoqueijo/android/instagramclone/likes/LikesActivity.java
@@ -22,8 +22,8 @@ public class LikesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        //setupBottomNavigationView();
+        setContentView(R.layout.activity_home);
+        setupBottomNavigationView();
     }
 
     /**
---
+++ app/src/main/java/com/nicoqueijo/android/instagramclone/login/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.nicoqueijo.android.instagramclone.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nicoqueijo.android.instagramclone.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
