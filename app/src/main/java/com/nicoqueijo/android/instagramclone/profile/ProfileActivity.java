caed302a7ba332b5ed4bd8142db37e1b62c905d8
Nicolas Queijo <queijonicolas@gmail.com>
2018-10-15 22:02:15 -0400

    completed part 29

--- app/src/main/java/com/nicoqueijo/android/instagramclone/profile/ProfileActivity.java
+++ app/src/main/java/com/nicoqueijo/android/instagramclone/profile/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -37,72 +38,84 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        setupBottomNavigationView();
-        setupToolbar();
-        setupActivityWidgets();
-        setProfileImage();
-        tempGridSetup();
+
+        init();
+
+//        setupBottomNavigationView();
+//        setupToolbar();
+//        setupActivityWidgets();
+//        setProfileImage();
+//        tempGridSetup();
     }
 
-    private void tempGridSetup() {
-        ArrayList<String> imgURLs = new ArrayList<>();
-        imgURLs.add("https://i.redd.it/roa3d955pjp11.jpg");
-        imgURLs.add("https://i.redd.it/uhv43kxiujp11.jpg");
-        imgURLs.add("https://i.redd.it/qa5p8lk3qlp11.jpg");
-        imgURLs.add("https://i.redd.it/506yk6yjeso11.png");
-        imgURLs.add("https://i.redd.it/6t038che1kp11.jpg");
-        imgURLs.add("https://i.redd.it/za3v4aanijp11.jpg");
-        imgURLs.add("https://i.redd.it/d86usxbhekp11.jpg");
-        imgURLs.add("https://i.redd.it/e2w2qlamxhp11.jpg");
-        imgURLs.add("https://i.redd.it/zw9zi222okp11.jpg");
-        imgURLs.add("https://i.redd.it/sg8qwrqdrjp11.jpg");
-        imgURLs.add("https://i.imgur.com/Bc252Cv.jpg");
-        setupImageGrid(imgURLs);
+    private void init() {
+        Log.d(TAG, "init: inflating " + getString(R.string.profile_fragment));
+        ProfileFragment fragment = new ProfileFragment();
+        FragmentTransaction transaction = ProfileActivity.this.getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(getString(R.string.profile_fragment));
+        transaction.commit();
     }
 
-    private void setupImageGrid(ArrayList<String> imgURLs) {
-        GridView gridView = findViewById(R.id.gridView);
-        GridImageAdapter adapter = new GridImageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
-        int gridWidth = getResources().getDisplayMetrics().widthPixels;
-        int imageWidth = gridWidth / NUM_GRID_COLUMNS;
-        gridView.setColumnWidth(imageWidth);
-        gridView.setAdapter(adapter);
-    }
-
-    private void setProfileImage() {
-        String imgURL = "o.aolcdn.com/images/dims?quality=100&image_uri=http%3A%2F%2Fo.aolcdn.com%2Fhss%2Fstorage%2Fmidas%2F965fc84051391e2ed53ce19881178931%2F202486815%2F10%2Bam%2BOfficial%2BAnnounce_G_FB%2Bcopy.jpg&client=amp-blogside-v2&signature=6a6cab645d24897df83fe4a2fc399f58891167ca";
-        UniversalImageLoader.setImage(imgURL, profilePhoto, mProgressBar, "https://");
-    }
-
-    private void setupActivityWidgets() {
-        mProgressBar = findViewById(R.id.profileProgressBar);
-        mProgressBar.setVisibility(View.GONE);
-        profilePhoto = findViewById(R.id.profile_photo);
-    }
-
-    private void setupToolbar() {
-        Toolbar toolbar = findViewById(R.id.profileToolbar);
-        setSupportActionBar(toolbar);
-        ImageView profileMenu = findViewById(R.id.profileMenu);
-        profileMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    /**
-     * BottomNavigationView setup
-     */
-    private void setupBottomNavigationView() {
-        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
-        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
-        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
-        Menu menu = bottomNavigationViewEx.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
-    }
+//    private void tempGridSetup() {
+//        ArrayList<String> imgURLs = new ArrayList<>();
+//        imgURLs.add("https://i.redd.it/roa3d955pjp11.jpg");
+//        imgURLs.add("https://i.redd.it/uhv43kxiujp11.jpg");
+//        imgURLs.add("https://i.redd.it/qa5p8lk3qlp11.jpg");
+//        imgURLs.add("https://i.redd.it/506yk6yjeso11.png");
+//        imgURLs.add("https://i.redd.it/6t038che1kp11.jpg");
+//        imgURLs.add("https://i.redd.it/za3v4aanijp11.jpg");
+//        imgURLs.add("https://i.redd.it/d86usxbhekp11.jpg");
+//        imgURLs.add("https://i.redd.it/e2w2qlamxhp11.jpg");
+//        imgURLs.add("https://i.redd.it/zw9zi222okp11.jpg");
+//        imgURLs.add("https://i.redd.it/sg8qwrqdrjp11.jpg");
+//        imgURLs.add("https://i.imgur.com/Bc252Cv.jpg");
+//        setupImageGrid(imgURLs);
+//    }
+//
+//    private void setupImageGrid(ArrayList<String> imgURLs) {
+//        GridView gridView = findViewById(R.id.gridView);
+//        GridImageAdapter adapter = new GridImageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
+//        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+//        int imageWidth = gridWidth / NUM_GRID_COLUMNS;
+//        gridView.setColumnWidth(imageWidth);
+//        gridView.setAdapter(adapter);
+//    }
+//
+//    private void setProfileImage() {
+//        String imgURL = "o.aolcdn.com/images/dims?quality=100&image_uri=http%3A%2F%2Fo.aolcdn.com%2Fhss%2Fstorage%2Fmidas%2F965fc84051391e2ed53ce19881178931%2F202486815%2F10%2Bam%2BOfficial%2BAnnounce_G_FB%2Bcopy.jpg&client=amp-blogside-v2&signature=6a6cab645d24897df83fe4a2fc399f58891167ca";
+//        UniversalImageLoader.setImage(imgURL, profilePhoto, mProgressBar, "https://");
+//    }
+//
+//    private void setupActivityWidgets() {
+//        mProgressBar = findViewById(R.id.profileProgressBar);
+//        mProgressBar.setVisibility(View.GONE);
+//        profilePhoto = findViewById(R.id.profile_photo);
+//    }
+//
+//    private void setupToolbar() {
+//        Toolbar toolbar = findViewById(R.id.profileToolbar);
+//        setSupportActionBar(toolbar);
+//        ImageView profileMenu = findViewById(R.id.profileMenu);
+//        profileMenu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//
+//    /**
+//     * BottomNavigationView setup
+//     */
+//    private void setupBottomNavigationView() {
+//        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+//        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
+//        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+//        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+//        Menu menu = bottomNavigationViewEx.getMenu();
+//        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+//        menuItem.setChecked(true);
+//    }
 }
---
+++ app/src/main/java/com/nicoqueijo/android/instagramclone/profile/ProfileFragment.java
@@ -0,0 +1,24 @@
+package com.nicoqueijo.android.instagramclone.profile;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nicoqueijo.android.instagramclone.R;
+
+public class ProfileFragment extends Fragment {
+
+    private static final String TAG = ProfileFragment.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
+    }
+}
