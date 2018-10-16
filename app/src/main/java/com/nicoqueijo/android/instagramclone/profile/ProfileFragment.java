c7e50625a51ae00f203c08d48a17c88e3dc7556f
Nicolas Queijo <queijonicolas@gmail.com>
2018-10-15 23:12:25 -0400

    completed part 30

--- app/src/main/java/com/nicoqueijo/android/instagramclone/profile/ProfileFragment.java
+++ app/src/main/java/com/nicoqueijo/android/instagramclone/profile/ProfileFragment.java
@@ -1,24 +1,94 @@
 package com.nicoqueijo.android.instagramclone.profile;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nicoqueijo.android.instagramclone.R;
+import com.nicoqueijo.android.instagramclone.utils.BottomNavigationViewHelper;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
 
     private static final String TAG = ProfileFragment.class.getSimpleName();
 
+    private static final int ACTIVITY_NUM = 4;
+
+    private TextView mPosts;
+    private TextView mFollowers;
+    private TextView mFollowing;
+    private TextView mDisplayName;
+    private TextView mUsername;
+    private TextView mWebsite;
+    private TextView mDescription;
+    private ProgressBar mProgressBar;
+    private CircleImageView mProfilePhoto;
+    private GridView mGridView;
+    private Toolbar mToolbar;
+    private ImageView mProfileMenu;
+    private BottomNavigationViewEx mBottomNavigationView;
+
+    private Context mContext;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-
+        mDisplayName = view.findViewById(R.id.display_name);
+        mUsername = view.findViewById(R.id.username);
+        mWebsite = view.findViewById(R.id.website);
+        mDescription = view.findViewById(R.id.description);
+        mProfilePhoto = view.findViewById(R.id.profile_photo);
+        mPosts = view.findViewById(R.id.tvPosts);
+        mFollowers = view.findViewById(R.id.tvFollowers);
+        mFollowing = view.findViewById(R.id.tvFollowing);
+        mProgressBar = view.findViewById(R.id.profileProgressBar);
+        mGridView = view.findViewById(R.id.gridView);
+        mToolbar = view.findViewById(R.id.profileToolbar);
+        mProfileMenu = view.findViewById(R.id.profileMenu);
+        mBottomNavigationView = view.findViewById(R.id.bottomNavViewBar);
+        mContext = getActivity();
+        setupBottomNavigationView();
+        setupToolbar();
         return view;
+    }
+
+    private void setupToolbar() {
+        ((ProfileActivity) getActivity()).setSupportActionBar(mToolbar);
+        mProfileMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewHelper.setupBottomNavigationView(mBottomNavigationView);
+        BottomNavigationViewHelper.enableNavigation(mContext, mBottomNavigationView);
+        Menu menu = mBottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
     }
 }
