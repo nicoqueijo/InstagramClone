c0f9f745463ececa9278e73f3a32287eda0b2b8e
Nicolas Queijo <queijonicolas@gmail.com>
2018-10-16 23:32:17 -0400

    completed part 41

--- app/src/main/java/com/nicoqueijo/android/instagramclone/share/ShareActivity.java
+++ app/src/main/java/com/nicoqueijo/android/instagramclone/share/ShareActivity.java
@@ -1,8 +1,10 @@
 package com.nicoqueijo.android.instagramclone.share;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -11,11 +13,13 @@ import android.view.MenuItem;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nicoqueijo.android.instagramclone.R;
 import com.nicoqueijo.android.instagramclone.utils.BottomNavigationViewHelper;
+import com.nicoqueijo.android.instagramclone.utils.Permissions;
 
 public class ShareActivity extends AppCompatActivity {
 
     private static final String TAG = ShareActivity.class.getSimpleName();
     private static final int ACTIVITY_NUM = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
     private Context mContext = ShareActivity.this;
 
@@ -25,7 +29,36 @@ public class ShareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: started");
 
-        setupBottomNavigationView();
+        if (checkPermissionsArray(Permissions.PERMISSIONS)) {
+
+        } else {
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
+        //setupBottomNavigationView();
+    }
+
+    public void verifyPermissions(String[] permissions) {
+        ActivityCompat.requestPermissions(ShareActivity.this, permissions, VERIFY_PERMISSIONS_REQUEST);
+    }
+
+    public boolean checkPermissionsArray(String[] permissions) {
+        for (int i = 0; i < permissions.length; i++) {
+            String check = permissions[i];
+            if (!checkPermission(check)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkPermission(String permission) {
+        int permissionRequest = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+        if (permissionRequest != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     /**
---
+++ app/src/main/java/com/nicoqueijo/android/instagramclone/utils/Permissions.java
@@ -0,0 +1,12 @@
+package com.nicoqueijo.android.instagramclone.utils;
+
+import android.Manifest;
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+}
