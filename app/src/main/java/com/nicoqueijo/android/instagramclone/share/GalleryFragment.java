82920bf9e086ae1f11ec458ecf5c8273b549a818
Nicolas Queijo <queijonicolas@gmail.com>
2018-10-17 15:09:42 -0400

    completed part 45

--- app/src/main/java/com/nicoqueijo/android/instagramclone/share/GalleryFragment.java
+++ app/src/main/java/com/nicoqueijo/android/instagramclone/share/GalleryFragment.java
@@ -7,6 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.nicoqueijo.android.instagramclone.R;
 
@@ -14,10 +19,36 @@ public class GalleryFragment extends Fragment {
 
     private static final String TAG = GalleryFragment.class.getSimpleName();
 
+    // widgets
+    private GridView mGridView;
+    private ImageView mGalleryImage;
+    private ProgressBar mProgressBar;
+    private Spinner mDirectorySpinner;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        mGridView = view.findViewById(R.id.gridView);
+        mGalleryImage = view.findViewById(R.id.galleryImageView);
+        mProgressBar = view.findViewById(R.id.progressBar);
+        mDirectorySpinner = view.findViewById(R.id.spinnerDirectory);
+
+        ImageView shareClose = view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+
+        TextView nextScreen = view.findViewById(R.id.tvNext);
+        nextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
     }
