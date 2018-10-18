1037ab699d177869a9bd27bdce95600e491f8741
Nicolas Queijo <queijonicolas@gmail.com>
2018-10-18 12:07:50 -0400

    completed part 53

--- app/src/main/java/com/nicoqueijo/android/instagramclone/utils/FirebaseMethods.java
+++ app/src/main/java/com/nicoqueijo/android/instagramclone/utils/FirebaseMethods.java
@@ -1,6 +1,7 @@
 package com.nicoqueijo.android.instagramclone.utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
@@ -15,6 +16,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.nicoqueijo.android.instagramclone.R;
 import com.nicoqueijo.android.instagramclone.models.User;
 import com.nicoqueijo.android.instagramclone.models.UserAccountSettings;
@@ -53,6 +55,11 @@ public class FirebaseMethods {
             String user_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
             StorageReference storageReference = mStorageReference
                     .child(filePaths.FIREBASE_IMAGE_STORAGE + "/" + user_id + "/photo" + (count + 1));
+            // convert imgUrl to bitmap
+            Bitmap bm = ImageManager.getBitmap(imgUrl);
+            byte[] bytes = ImageManager.getBytesFromBitmap(bm, 100);
+            UploadTask uploadTask = null;
+            uploadTask = storageReference.putBytes(bytes);
         }
         // case 2: new profile photo
         else if (photoType.equals(mContext.getString(R.string.profile_photo))) {
